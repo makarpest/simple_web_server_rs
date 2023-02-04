6a677d2d4e19c7cb00e944266f879c44e3b69c57
makarpest <makarpestrikov04@gmail.com>
2023-02-04 21:32:27 +0300

    Result

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -13,11 +13,14 @@ fn main() {
 
         pool.execute(|| handle_connection(stream));
     }
+
+    println!("Выключение.");
 }
 
+
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = vec![0; 512];
-    stream.read_exact(&mut buffer).unwrap();
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -27,7 +30,6 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
 
     } else if buffer.starts_with(sleep) {
-
         thread::sleep(std::time::Duration::from_secs(5));
 
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
